2870de2e59e32d2ba967d80a4e1e908de7b312fe
Julian Lopez <julian.lopez.mt@correo.usa.edu.co>
2021-11-28 15:35:08 -0500

    reto 1

---
+++ app/src/main/java/com/example/reto11/ui/productos/ProductosFragment.java
@@ -0,0 +1,8 @@
+package com.example.reto11.ui.productos;
+
+import androidx.fragment.app.Fragment;
+
+public class ProductosFragment extends Fragment {
+
+
+}
---
+++ app/src/main/java/com/example/reto11/ui/productos/ProductosModel.java
@@ -0,0 +1,7 @@
+package com.example.reto11.ui.productos;
+
+import androidx.lifecycle.ViewModel;
+
+public class ProductosModel extends ViewModel {
+
+}
